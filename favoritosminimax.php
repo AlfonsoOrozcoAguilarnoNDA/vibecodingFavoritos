3fff0643b42d283dee0c3a37e4f0270251864f65
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-13 15:10:40 -0600

    Create favoritosminimax.php

---
+++ favoritosminimax.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hola mundo";
+?>
+EL de Minimax estane licencia lgpl pero puedes verlo aqui, es el mejor de los LLM
+https://github.com/AlfonsoOrozcoAguilarnoNDA/lemkotir/blob/main/favoritos.php

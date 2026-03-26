80e09f0b07ac1220c9589dddbd18c1e2b66049bd
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-26 17:21:51 -0600

    Update favoritosgemini.php
    Se cambia Un CDN

--- favoritosgemini.php
+++ favoritosgemini.php
@@ -69,7 +69,7 @@ $edit_link = null; if (isset($_GET['edit_link'])) { $res = mysqli_query($link, "
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gotham Metro Favs</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/css/all.min.css" crossorigin="anonymous">
     <style>
         body { background: #111; color: #eee; font-family: 'Segoe UI', sans-serif; padding-bottom: 80px; }
         .metro-tile { 
